7e6d1490e0b56d2f68bd5fbd2ebae9bd4a7bef6d
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-16 20:52:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.20.10.0/24"
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "tfc-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.20.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -21,9 +49,15 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
+  
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+  
   tags = {
     Name                 = var.instance_name
     "Linux Distribution" = "Ubuntu"
   }
 }
+
