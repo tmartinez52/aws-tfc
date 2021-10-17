677ec44ffe490ef6f0907322248561f4f0fc8eaa
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-16 20:52:38 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "region" {
   default     = "us-west-1"
 }
 
+variable "availability_zone" {
+  description = "AWS AZ"
+  default     = "us-west-1a"
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
