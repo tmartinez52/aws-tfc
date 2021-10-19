8c914506747d0f89a6442c9caab863941e122a89
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-10-19 05:01:49 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_cloud9_environment_ec2" "cloud9_tfc" {
   name = var.instance_name
   instance_type = var.instance_type
+  depends_on [aws_vpc.myvpc]
 }
 
 data "aws_instance" "cloud9_instance" {
@@ -12,6 +13,28 @@ data "aws_instance" "cloud9_instance" {
     name = "tag:aws:cloud9:environment"
     values = [
       aws_cloud9_environment_ec2.cloud9_tfc.id]
+  }
+}
+
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.vpc_tag
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.my_subnet
+  availability_zone = var.availability_zone
+  
+  map_public_ip_on_launch = true
+  
+  
+  tags = {
+    Name = "tfc-example"
   }
 }
 
