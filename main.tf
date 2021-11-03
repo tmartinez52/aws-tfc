6ae60cf5d1327de4dcd9b2dd3dcd35c03cb86fa8
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-02 19:20:28 -0600

    Update main.tf
    updating user_data

--- main.tf
+++ main.tf
@@ -110,8 +110,8 @@ resource "aws_instance" "web-server-instance" {
 
     user_data = <<-EOF
                 #!/bin/bash
-                sudo apt-update -y
-                sudo apt-install apache2 -y
+                sudo apt update -y
+                sudo apt install apache2 -y
                 sudo systemctl start apache2
                 sudo bash -c "echo your very first web server  > /var/www/html/index.html'
                 EOF
