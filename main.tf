862b2a9b673499c11d67be85f0b105bcb55ef54e
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-02 18:42:27 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,121 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_block_range}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags {
-    "Environment" = "${var.environment_tag}"
-  }
-}resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  tags {
-    "Environment" = "${var.environment_tag}"
-  }
-}resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.subnet1_cidr_block_range}"
-  map_public_ip_on_launch = "true"
-  availability_zone = "${var.availability_zone}"
-  tags {
-    "Environment" = "${var.environment_tag}"
-    "Type" = "Public"
-  }
-}resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
-  }tags {
-    "Environment" = "${var.environment_tag}"
-  }
-}resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
-}resource "aws_key_pair" "ec2key" {
-  key_name = "publicKey"
-  public_key = "${file(var.public_key_path)}"
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    
+    tags = {
+        Name = "Production"
+    }
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "Production"
+    }
+}
+
+resource "aws_route_table" "prod-route-table" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+    tags = {
+        Name = "Prod"
+    }
+}
+
+resource "aws_subnet" "subnet-one" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "Prod"
+    }
+}
+
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.subnet-one.id
+    route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_security_group" "allow_web" {
+    name = "allow_web"
+    description = "Allow Web inbound traffic"
+    vpc_id  = aws_vpc.main.id
+
+    ingress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "Allow Web"
+    }
+}
+
+resource "aws_network_interface" "web-server-nic" {
+    subnet_id = aws_subnet.subnet-one.id 
+    private_ips = ["10.0.1.50"]
+    security_groups = [aws_security_group.allow_web.id] 
+}
+
+resource "aws_eip" "one" {
+    vpc = true
+    network_interface = aws_network_interface.web-server-nic.id
+    associate_with_private_ip = "10.0.1.50"
+
+    depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" "web-server-instance" {
+    ami =  "ubuntu ami"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = "main-key"
+
+    network_interface {
+        device_index = 0 
+        network_interface_id = aws_network_interface.web-server-nic.id
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-update -y
+                sudo apt-install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c "echo your very first web server  > /var/www/html/index.html'
+                EOF
+    tags = {
+        Name = "web-server"
+    }
 }
